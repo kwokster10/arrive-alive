10e871d5b41a39a83f6890ac39af6e5477b8429d
Julie Kwok <kwokster10@hotmail.com>
2017-06-18 22:19:54 -0400

    Adds scope and validation of user emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  default_scope { order(created_at: :desc) }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
@@ -11,6 +13,7 @@ class User < ActiveRecord::Base
   validates :phone_number,
             format: { with: /\d{3}-?\d{3}-?\d{4}/, message: I18n.t('phone_number_error') }
   validates_presence_of :name
+  validates_uniqueness_of :email
 
   def owner_of?(record)
     id == record.user_id
