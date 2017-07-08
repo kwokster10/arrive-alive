39001f1be9415eb93b99133dd0fcc33b3f30abd2
Julie Kwok <kwokster10@hotmail.com>
2017-07-08 18:55:13 -0400

    Forces SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -92,6 +92,7 @@ Rails.application.configure do
     authentication: 'plain',
     enable_starttls_auto: true,
     user_name: ENV['AA_GMAIL_USERNAME'],
-    password: ENV['AA_GMAIIL_APP_PASSWORD']
+    password: ENV['AA_GMAIIL_APP_PASSWORD'],
+    tls: true
   }
 end
